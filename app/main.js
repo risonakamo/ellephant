e47294a01bc717619e36e85ace79feae736039bb
risonakamo <hmykanae@gmail.com>
2017-06-06 13:08:08 -0400

    initial commit electron setup

---
+++ app/main.js
@@ -0,0 +1,18 @@
+const {app,BrowserWindow,session}=require("electron");
+const path=require("path");
+const url=require("url");
+
+var _win;
+
+function main()
+{
+    _win=new BrowserWindow({width:1280,height:720});
+
+    _win.loadURL(url.format({
+        pathname:path.join(__dirname,"index.html"),
+        protocol:"file:",
+        slashes:true
+    }));
+}
+
+app.on("ready",main);
