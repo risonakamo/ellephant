57e78b901cfadbb325ba62491ba64868ed6f7292
risonakamo <hmykanae@gmail.com>
2017-06-08 01:41:00 -0400

    update expedition on receivedeck

--- app/index.js
+++ app/index.js
@@ -41,21 +41,31 @@ function ipcReceivers()
     ipcRenderer.on("portinfo",(err,res)=>{
         portUpdate(res);
     });
+
+    ipcRenderer.on("deckinfo",(err,res)=>{
+        expeditionUpdate(res.api_data);
+    });
 }
 
 function portUpdate(port)
 {
+    expeditionUpdate(port.api_data.api_deck_port);
+}
+
+//needs to be given api_deck_port from the port object, an array
+function expeditionUpdate(data)
+{
     for (var x=1;x<=3;x++)
     {
-        if (port.api_data.api_deck_port[x].api_mission[0]==0)
+        if (data[x].api_mission[0]==0)
         {
             _expFleets[x-1].clearExpedition();
             continue;
         }
 
         _expFleets[x-1].loadExp({
-            expNum:port.api_data.api_deck_port[x].api_mission[1],
-            timeComplete:port.api_data.api_deck_port[x].api_mission[2]
+            expNum:data[x].api_mission[1],
+            timeComplete:data[x].api_mission[2]
         });
-    }
+    }    
 }
--- app/main.js
+++ app/main.js
@@ -16,16 +16,16 @@ function main()
     _win.loadURL(`file://${__dirname}/index.html`);
 }
 
-function receivePort(par)
+function receiveArb(par,channel)
 {
     _gamewindow.webContents.debugger.sendCommand("Network.getResponseBody",{requestId:par.requestId},(err,res)=>{
         if (res.body==undefined)
         {
-            setTimeout(()=>{receivePort(par)},500);
+            setTimeout(()=>{receiveArb(par,channel)},500);
             return;
         }
 
-        _win.webContents.send("portinfo",(JSON.parse(res.body.slice(7))));
+        _win.webContents.send(channel,(JSON.parse(res.body.slice(7))));
     });
 }
 
@@ -42,7 +42,12 @@ function ipcs()
             {
                 if (par.response.url=="http://203.104.209.39/kcsapi/api_port/port")
                 {
-                    setTimeout(()=>{receivePort(par)},500);
+                    setTimeout(()=>{receiveArb(par,"portinfo")},500);
+                }
+
+                if (par.response.url=="http://203.104.209.39/kcsapi/api_get_member/deck")
+                {
+                    setTimeout(()=>{receiveArb(par,"deckinfo")},500);
                 }
             }
         });
@@ -58,6 +63,32 @@ function ipcs()
 }
 
 app.on("ready",main);
+
+// function receivePort(par)
+// {
+//     _gamewindow.webContents.debugger.sendCommand("Network.getResponseBody",{requestId:par.requestId},(err,res)=>{
+//         if (res.body==undefined)
+//         {
+//             setTimeout(()=>{receivePort(par)},500);
+//             return;
+//         }
+
+//         _win.webContents.send("portinfo",(JSON.parse(res.body.slice(7))));
+//     });
+// }
+
+// function receiveDeck(par)
+// {
+//     _gamewindow.webContents.debugger.sendCommand("Network.getResponseBody",{requestId:par.requestId},(err,res)=>{
+//         if (res.body==undefined)
+//         {
+//             setTimeout(()=>{receivePort(par)},500);
+//             return;
+//         }
+
+//         _win.webContents.send("deckinfo",(JSON.parse(res.body.slice(7))));
+//     });    
+// }
 
 // function gameWindowDebugSetup()
 // {
