265286552dbc00ae5526a4826deed2dd4c63c94e
risonakamo <hmykanae@gmail.com>
2017-06-07 21:31:07 -0400

    +port expedition updates

--- app/index.js
+++ app/index.js
@@ -13,10 +13,12 @@ function setupInput()
     var goBt=document.querySelector(".go-bt");
     var urlBox=document.querySelector(".url-box");
     var inputWrap=document.querySelector(".input-wrap");
+    var viewer=document.querySelector(".viewer");
 
     var sendWindowRequest=function(e){
         ipcRenderer.send("requestWindow",urlBox.value);
         inputWrap.parentNode.removeChild(inputWrap);
+        viewer.classList.remove("collapse");
     };
 
     goBt.addEventListener("click",sendWindowRequest);
@@ -39,5 +41,19 @@ function ipcReceivers()
 
 function portUpdate(port)
 {
+    var expFleets=document.querySelectorAll("exp-fleet");
+    
+    for (var x=1;x<=3;x++)
+    {
+        if (port.api_data.api_deck_port[x].api_mission[0]==0)
+        {
+            expFleets[x-1].clearExpedition();
+            continue;
+        }
 
+        expFleets[x-1].loadExp({
+            expNum:port.api_data.api_deck_port[x].api_mission[1],
+            timeComplete:port.api_data.api_deck_port[x].api_mission[2]
+        });
+    }
 }
