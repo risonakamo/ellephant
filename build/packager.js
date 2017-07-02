9069f546baf0652af1021c5c782bca54b708d8fa
risonakamo <hmykanae@gmail.com>
2017-07-02 17:04:22 -0400

    removed dev electron build stuff

---
+++ build/packager.js
@@ -0,0 +1,8 @@
+const packager=require("electron-packager");
+
+packager({
+    dir:`${__dirname}/..`,
+    tmpdir:`${__dirname}/temp`,
+    electronVersion:"1.6.11",
+    ignore:["build","face-check",/(.*)\.h$/,"ref"]
+},(err,path)=>{});
