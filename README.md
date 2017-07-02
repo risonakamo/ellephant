ee30530e9008b80ef3a780bd9eab2d050082b0b1
risonakamo <hmykanae@gmail.com>
2017-07-02 18:08:00 -0400

    changed name because it was take :( new elegant name ELLEPHANT

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+### Kancolle Ellephant Viewer
 simple viewer for Kancolle with focus on aesthetics and functionality for everyday actions
 
 ![](http://i.imgur.com/2vJwcCS.png)
+<img src="http://i.imgur.com/cDZTNMQ.png" width="500px">
--- build/packager.js
+++ build/packager.js
@@ -4,5 +4,5 @@ packager({
     dir:`${__dirname}/..`,
     tmpdir:`${__dirname}/temp`,
     electronVersion:"1.6.11",
-    ignore:["build","face-check",/(.*)\.h$/,"ref"]
+    ignore:["build","face-check",/(.*)\.h$/,"ref",/(.*)\.md$/]
 },(err,path)=>{});
