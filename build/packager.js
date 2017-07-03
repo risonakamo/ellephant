a16180b959ae9736add651b855b71bdd9ea5f16d
risonakamo <hmykanae@gmail.com>
2017-07-03 02:37:25 -0400

    reorganise viewer files

--- build/packager.js
+++ build/packager.js
@@ -4,5 +4,5 @@ packager({
     dir:`${__dirname}/..`,
     tmpdir:`${__dirname}/temp`,
     electronVersion:"1.6.11",
-    ignore:["build","face-check",/(.*)\.h$/,"ref",/(.*)\.md$/]
+    ignore:["build","face-check",/(.*)\.h$/,"ref",/(.*)\.md$/,"viewer/js/h"]
 },(err,path)=>{});
